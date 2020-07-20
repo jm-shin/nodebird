a653cb10533d90ba4367ea79a2b1b2e1e830b276
jongmin <ace4463@gmail.com>
2020-07-20 18:03:18 +0900

    카카오톡 로그인 구현

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+
 require('dotenv').config();
 
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
 sequelize.sync();
-
+passportConfig(passport);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -31,9 +36,13 @@ app.use(session({
     secure: false,
   },
 }));
+
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
